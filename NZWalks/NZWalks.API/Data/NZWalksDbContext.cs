fbc77be849b0a94cf50b7392771425ea986a272f
ranjurave <ranjurave@gmail.com>
2022-10-20 21:41:16 +1300

    section2 end

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+        //Create tables in database if not exist
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }    
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation property can be any number of Walks
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
