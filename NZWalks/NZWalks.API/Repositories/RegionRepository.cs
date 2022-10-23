d87928ac5b5a55314f46ddd79b47a3c4c467b833
ranjurave <ranjurave@gmail.com>
2022-10-23 16:48:47 +1300

    DTO hiding data model implemented with comment

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nzWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext NzWalksDbContext)
+        {
+            nzWalksDbContext = NzWalksDbContext;
+        }
+
+        public IEnumerable<Region> GetAll()
+        {
+            return nzWalksDbContext.Regions.ToList();
+        }
+    }
+}
